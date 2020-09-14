0bd0e545ec165ed8274f0fd7543c84d9c8cd627a
Jorge Cardona <jcardonac@intergrupo.com>
2020-09-14 13:49:38 -0500

    datatables impl

--- src/app/components/admin/catalogproducts/query-product/query-product.component.ts
+++ src/app/components/admin/catalogproducts/query-product/query-product.component.ts
@@ -4,6 +4,8 @@ import { CollectionsService } from '../../../../services/collections.service';
 import Swal from 'sweetalert2';
 import { DataTableDirective } from 'angular-datatables';
 import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { _MatTabLinkBase } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-query-product',
@@ -11,92 +13,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./query-product.component.css']
 })
 export class QueryProductComponent implements OnInit {
-  dtColumnSearchingOptions: any = {};
+  dtOptions: any = {};
   @ViewChild(DataTableDirective)
   datatableElement: DataTableDirective;
   public data: any[];
-
-  title = 'angulardatatables';
-  dtOptions: DataTables.Settings = {};
-
-
   constructor(private productService: ProductService, private colleccionService: CollectionsService,
     private elRef: ElementRef,
     private router: Router,
     private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.getProducts()
 
-    this.colleccionService.Get('Products').subscribe(res => {
-      this.data = res
-    });
-
+        
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 5,
-      processing: true
+      processing: true,
+      columns:
+        [
+          {
+            title: 'Acciones',
+            className: 'notranslate',
+            render: function (data: any, type: any, full: any) {
+              return '<button type="button" class="btn btn-icon btn-rounded fas fa-edit" edit-data-id="' + full.productID + '"></button>';
+            }
+          }
+        ]
     };
   }
 
-  public getTranDocType() {
+
+  public getProducts() {
     try {
-      const that = this;
-      this.dtColumnSearchingOptions = {
-        ajax: (dataTablesParameters: any, callback) => {
-          this.colleccionService.Get('Products')
-            .subscribe(
-              res => {
-                callback({
-                  recordsTotal: 10,
-                  recordsFiltered: '',
-                  data: that
-                });
-              }
-            );
-        },
-        columns: [{
-          title: 'ID',
-          data: 'TranDocTypeID',
-          className: 'notranslate'
-        }, {
-          title: 'Nombre',
-          data: 'TranDocTypeDesc',
-          className: 'notranslate'
-        }, {
-          title: 'Tipo',
-          data: 'TranType',
-          className: 'dt-body-center notranslate'
-        }, {
-          title: 'Estado',
-          data: 'Inactive',
-          className: 'notranslate',
-          render: function (data: any, type: any, full: any) {
-            let estado = full.Inactive === true ? 'checked' : '';
-            if (estado)
-              return '<div class="checkbox checkbox-primary checkbox-fill m-r-10" style="display: table;margin-top: -29px;"><input type="checkbox" id="switch-a-2" value=' + estado + ' readonly="readonly" ' + estado + ' onclick="javascript: return false;"><label for="switch-a-2" class="cr" style="font-size:0px">' + full.Process + '</label></div>'
-            else
-              return '<div class="checkbox checkbox-primary checkbox-fill d-inline m-r-10"><input type="checkbox" id="switch-a-2" value=' + estado + ' readonly="readonly" ' + estado + ' onclick="javascript: return false;"><label for="switch-a-2" class="cr" style="font-size:0px">' + full.Process + '</label></div>'
-          }
-        }, {
-          title: 'Acciones',
-          className: 'notranslate',
-          render: function (data: any, type: any, full: any) {
-            return '<button type="button" class="btn btn-icon btn-rounded fas fa-edit" edit-data-id="' + full.TranDocTypeID + '"></button>';
-          }
-        }],
-        scrollY: "250px",
-        scrollCollapse: true,
-        responsive: true,
-        dom: 'lBfrtip',
-        buttons: [
-          'copy',
-          'print',
-          'excel',
-          'pdf'
-        ]
-      };
+      this.colleccionService.Get('Products').subscribe(res => {
+        this.data = res;
+      });
     } catch (error) {
       Swal.fire('Notificación', error, 'warning');
     }
+  }
+
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 }
--- src/app/services/collections.service.ts
+++ src/app/services/collections.service.ts
@@ -10,16 +10,11 @@ export class CollectionsService {
 
   API_URI = 'http://localhost:5000/api'
 
-  constructor(private http: HttpClient, 
-              private router: Router) { }
+  constructor(private http: HttpClient,
+    private router: Router) { }
 
 
-Get(url:string) {
-  const headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1OTkxNzEzMDMsImV4cCI6MTU5OTI1NzcwMywiaWF0IjoxNTk5MTcxMzAzfQ.q6IGzHkY_rBBNoUcSN5jebPTGwkpf5QpZUJIIk3vNGk'
-    // 'Authorization': 'bearer '+sessionStorage.getItem('fgh0x01b4#8')
-  });
+  Get(url: string) {
     return this.http.get<any[]>(`${this.API_URI}/${url}`);
   }
 }
